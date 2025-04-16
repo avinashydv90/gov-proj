94b8af994868d92e677dc6a5bc62e5dbb4c703f6
vishal kannauzia <kannauziavishal0@gmail.com>
2025-04-16 17:09:18 +0530

    upscal images add

---
+++ SPA/src/constants/Prakalp karyalay adhikari.ts
@@ -0,0 +1,59 @@
+export const PrakalpKaryalayAdhikari = [
+    {
+      id: 1,
+      officerName: "श्री.राजेंद्रकुमार हौसाबाई भानुदास हिवाळे",
+      title: "प्रकल्प अधिकारी",
+      Designation: "Project Officer",
+      Contact: "9850683504",
+      Email: "rajendra.hiwale@mah.gov.in"
+    },
+    {
+      id: 2,
+      officerName: "श्री.दिवाकर गौकर्णा देविदास काळपांडे",
+      title: "सहाय्यक प्रकल्प अधिकारी (प्रशासन / नियोजन)",
+      Designation: "Assistant Project Officer",
+      Contact: "7703099991",
+      Email: "diwakar.kalpande23@mah.gov.in"
+    },
+    {
+      id: 3,
+      officerName: "श्री.अरविंद सरस्वती रामलाल जाधव",
+      title: "सहाय्यक प्रकल्प अधिकारी (विकास)",
+      Designation: "Assistant Project Officer",
+      Contact: "9421375779",
+      Email: "arvind.jadhav16@mah.gov.in"
+    },
+    {
+      id: 4,
+      officerName: "श्री.पंढरीनाथ लक्ष्मीबाई नारायण वेखंडे",
+      title: "लेखा अधिकारी",
+      Designation: "Accounting Officer",
+      Contact: "8169001730",
+      Email: "v.pandharinath2026@mheauth.nic.in"
+    },
+    {
+      id: 5,
+      officerName: "श्री.बापुराव वत्सलाबाई रामा जाधव",
+      title: "सहाय्यक प्रकल्प अधिकारी (शिक्षण)",
+      Designation: "Assistant Project Officer",
+      Contact: "9271386687",
+      Email: "brjadhav16@mah.gov.in"
+    },
+    {
+      id: 6,
+      officerName: "श्री.सुनिलसींग अनुसया नवलसींग पाटील",
+      title: "कार्यालय अधिक्षक",
+      Designation: "Office Supritendent",
+      Contact: "9224023840",
+      Email: "patil.sunilsing2025@mheauth.nic.in"
+    },
+    {
+      id: 7,
+      officerName: "श्री.विलास जनाबाई नामदेव चन्ने",
+      title: "कार्यालय अधिक्षक",
+      Designation: "Office Supritendent",
+      Contact: "9594052965",
+      Email: "channe.vilas2032@mheauth.nic.in"
+    }
+  ];
+  
